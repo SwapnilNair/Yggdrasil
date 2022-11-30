ff0300c1dc02100ab2fd64ed5b0405dfac332817
Shrinithi Natarajan <kavinattu2001@gmail.com>
2022-12-01 00:09:05 +0530

    description of class functions in brief

--- src/norse.py
+++ src/norse.py
@@ -22,7 +22,13 @@ class Norse():
         self._socketStorage = None
         self._leaderHeimdall = None
         
-    def sendMessage(self, topic, time):
+    def sendMessage(self, topic):
+        '''
+            sends each batch of incoming messages to the leader broker. 
+            checks simultaneously if the current leader broker is alive. 
+            If not, calls checkForLeaderHeimdall() until the current leader's host and port is obtained from the zookeeper
+            sends the batch of messages to the current leader broker through TCP socket.
+        '''
         self._socketStorage = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self._socketStorage.connect((self._leaderHeimdall["heimdallIp"], self._leaderHeimdall["heimdallPort"]))
@@ -38,6 +44,12 @@ class Norse():
         
         
     def produceMessage(self, topic, message):
+        '''
+            if topic doesnt exist already, it creates a new list for that topic. 
+            Subsequent messages are added to respective topics along with timestamp until buffer capacity is reached.
+            if the capacity is reached, it sends that batch to sendMessage() and clears that buffer of that particular topic
+            the remaining contents of the buffers are sent to sendmessage() is no other messages are being read within 5 seconds.
+        '''
         if self._bufferForceFlushTimer != None:
             self._bufferForceFlushTimer.cancel()
         if not self._incomingMessages.get(topic):
@@ -62,16 +74,8 @@ class Norse():
     def checkForLeaderHeimdall(self):
         URL = "http://localhost:5000/leader"
         """
-        {
-            "leader": -1
-        }
-        {
-            "leader": {
-                "heimdallId" : "",
-                "heimdallIp" : "",
-                "heimdallPort" : ""
-            }
-        }
+        Until zookeeper labels leader as -1, keep requesting zookeeper to send the metadata of the current leader broker
+        once received, parse the json object to retrieve ip and port of the current leader broker.
         """
         leader = -1
         while (leader == -1 or leader == "-1"):
