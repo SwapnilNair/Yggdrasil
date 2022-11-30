74014d1ff8418d5c4966cfa48f3cd4c8716dc1d2
saisamarthtaluri <spacexpedia@gmail.com>
2022-12-01 00:17:51 +0530

    Implemented request function for consumer

--- src/asgardian.py
+++ src/asgardian.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 import json
 from _thread import *
 import threading
+import time
 
 lock = threading.Lock()
 
@@ -22,8 +23,6 @@ class Asgardian():
         sock.send(dataString.encode('utf-8'))
         sock.close()
 
-
-
     def subscribe(self, topics):
         #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         HOST = 'localhost'
@@ -35,23 +34,41 @@ class Asgardian():
         broadcastThreadHeimdall2 = threading.Thread(target=self.broadcastHandler, args=(HOST, PORT_2, topics))
         broadcastThreadHeimdall0.start()
         broadcastThreadHeimdall1.start()
-        broadcastThreadHeimdall2.start()
-        
-                  
+        broadcastThreadHeimdall2.start()       
+
+    def BrokerHandler(self, HOST, PORT, message):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        dataString = json.dumps(self.wrapMessage({'message': message}))
+        sock.send(dataString.encode('utf-8'))
+        conn, addr = sock.accept()
+        print(f'Connected to: {addr[0]}:{str(addr[1])}')
+        while True:
+            data = conn.recv(4096).decode()
+            print(data)
+
             
-    def rcvMessage(self):    
-        
-        IP = 'localhost'
-        PORT = 8000
-        broker_addr = (IP,PORT)
-        sock.connect(broker_addr)
-        print('Broker connected')
-        print(sock.recv(4096).decode())
-        sock.close()
+    def request(self):      
+        HOST = 'localhost'
+        PORT_0 = 3500
+        PORT_1 = 3501
+        PORT_2 = 3502
+        reqMessage = 'RequestingMessage'
+        broadcastReqHeimdall1 = threading.Thread(target=self.BrokerHandler, args=(HOST, PORT_1, reqMessage))
+        broadcastReqHeimdall2 = threading.Thread(target=self.BrokerHandler, args=(HOST, PORT_2, reqMessage))
+        broadcastReqHeimdall0 = threading.Thread(target=self.BrokerHandler, args=(HOST, PORT_0, reqMessage))
+        broadcastReqHeimdall0.start()
+        broadcastReqHeimdall1.start()
+        broadcastReqHeimdall2.start() 
 
  
-    def wrapMessage(self, message):
-        return {"nodeType" : "asgardian", "ip" : self._ip, "port" : self._port, "nodeID" : str(self._id), "flag" : self._flag, "message" : message}
+    def wrapMessage(self, payload):
+        return {
+                "nodeType" : "asgardian",
+                "ip" : self._ip,
+                "port" : self._port,
+                "nodeID" : str(self._id),
+                "flag" : self._flag,
+                "message" : payload
+                }
             
-
-
